7cea8e9a60fb3767b0d45d64dbb05b58af0572d4
Guilherme Santos <gui_t900@hotmail.com>
2020-06-18 17:32:43 -0300

    frontend build

---
+++ build/precache-manifest.27a660b9090ad4a0e3c0b20908e65876.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1085d460d8b3f0e8869b1aedb3a4b8b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df22e3669f9471afa4bd",
+    "url": "/static/css/main.d9f6dae8.chunk.css"
+  },
+  {
+    "revision": "49bdc01c5b65f8d5ccd5",
+    "url": "/static/js/2.3b9df0ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3b9df0ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df22e3669f9471afa4bd",
+    "url": "/static/js/main.f9797a8a.chunk.js"
+  },
+  {
+    "revision": "68d5127cb03992670844",
+    "url": "/static/js/runtime-main.684b035f.js"
+  }
+]);
